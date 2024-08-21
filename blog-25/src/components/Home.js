4c91c8f86565078959d4a1380008a9474482f5ec
Iraku Harry <iraqooh@gmail.com>
2024-08-21 07:38:58 +0300

    Image render fix

--- blog-25/src/components/Home.js
+++ blog-25/src/components/Home.js
@@ -8,7 +8,7 @@ function Home() {
         <Row>
           <Col>
             <h2>Welcome to Our Project Blog</h2>
-            <p>This blog documents the contributions of each team member to the TBD project.</p>
+            <p>This blog documents the contributions of each team member to the Bidirectional Sign Language and Speech Translation System project.</p>
           </Col>
         </Row>
       </Container>
@@ -17,7 +17,7 @@ function Home() {
           <Row>
             <Col md={3}>
               <Card className="text-center">
-                <Card.Img variant="top" src={process.env.PUBLIC_URL + "/images/atong.jpg"} alt="Atong Kur Abraham" />
+                <Card.Img variant="top" src={`${process.env.PUBLIC_URL}/images/atong.JPG`} alt="Atong Kur Abraham" />
                 <Card.Body>
                   <Card.Title>Atong Kur Abraham</Card.Title>
                   <Card.Text>Lead Developer</Card.Text>
@@ -26,7 +26,7 @@ function Home() {
             </Col>
             <Col md={3}>
               <Card className="text-center">
-                <Card.Img variant="top" src={process.env.PUBLIC_URL + '/images/ddamulira.jpg'} alt="Ddamulira Owen" />
+                <Card.Img variant="top" src={`${process.env.PUBLIC_URL}/images/ddamulira.JPG`} alt="Ddamulira Owen" />
                 <Card.Body>
                   <Card.Title>Ddamulira Owen</Card.Title>
                   <Card.Text>ML Engineer</Card.Text>
@@ -35,7 +35,7 @@ function Home() {
             </Col>
             <Col md={3}>
               <Card className="text-center">
-                <Card.Img variant="top" src={process.env.PUBLIC_URL + "/images/mutumba.jpg"} alt="Mutumba Robert" />
+                <Card.Img variant="top" src={`${process.env.PUBLIC_URL}/images/mutumba.JPG`} alt="Mutumba Robert" />
                 <Card.Body>
                   <Card.Title>Mutumba Robert</Card.Title>
                   <Card.Text>Frontend Developer</Card.Text>
@@ -44,7 +44,7 @@ function Home() {
             </Col>
             <Col md={3}>
               <Card className="text-center">
-                <Card.Img variant="top" src={process.env.PUBLIC_URL + "/images/iraku.jpg"} alt="Iraku Harry" />
+                <Card.Img variant="top" src={`${process.env.PUBLIC_URL}/images/iraku.JPG`} alt="Iraku Harry" />
                 <Card.Body>
                   <Card.Title>Iraku Harry</Card.Title>
                   <Card.Text>Project Manager</Card.Text>
